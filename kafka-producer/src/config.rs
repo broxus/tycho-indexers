8aa85f32e4630915769524e60e0569a7c44e6a67
Vladimir Petrzhikovskii <hello@0xdeafbeef.dev>
2024-11-06 17:19:07 +0100

    initial commit

---
+++ kafka-producer/src/config.rs
@@ -0,0 +1,57 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct KafkaConsumerConfig {
+    pub topic: String,
+    pub brokers: String,
+    pub group_id: String,
+    #[serde(default)]
+    pub security_config: Option<SecurityConfig>,
+    #[serde(default = "default_session_timeout_ms")]
+    pub session_timeout_ms: u32,
+    #[serde(default = "default_offset_commit_interval_ms")]
+    pub message_timeout_ms: Option<u32>,
+    pub message_max_size: Option<usize>,
+}
+
+impl Default for KafkaConsumerConfig {
+    fn default() -> Self {
+        Self {
+            topic: "".to_string(),
+            brokers: "".to_string(),
+            group_id: "tycho-kafka-producer".to_string(),
+            security_config: None,
+            session_timeout_ms: default_session_timeout_ms(),
+            message_timeout_ms: default_offset_commit_interval_ms(),
+            message_max_size: Some(2_000_000),
+        }
+    }
+}
+
+fn default_offset_commit_interval_ms() -> Option<u32> {
+    Some(5000)
+}
+
+fn default_session_timeout_ms() -> u32 {
+    6000
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+#[serde(tag = "security_protocol")]
+pub enum SecurityConfig {
+    #[cfg(feature = "ssl")]
+    Ssl(SslConfig),
+}
+
+#[derive(Debug, Clone, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct SslConfig {
+    pub security_protocol: String,
+    pub ssl_ca_location: String,
+    pub ssl_key_location: String,
+    pub ssl_certificate_location: String,
+    #[serde(default)]
+    pub enable_ssl_certificate_verification: Option<bool>,
+}
---
+++ kafka-producer/src/subscriber.rs
@@ -0,0 +1,139 @@
+use std::time::Duration;
+
+use anyhow::Result;
+use everscale_types::boc::BocRepr;
+use everscale_types::cell::CellBuilder;
+use futures_util::future::BoxFuture;
+use futures_util::stream::FuturesOrdered;
+use futures_util::{FutureExt, StreamExt};
+use rdkafka::producer::FutureRecord;
+use tycho_block_util::block::BlockStuff;
+use tycho_core::block_strider::{StateSubscriber, StateSubscriberContext};
+
+use crate::config::KafkaConsumerConfig;
+
+pub struct KafkaProducer {
+    producer: rdkafka::producer::FutureProducer,
+    config: KafkaConsumerConfig,
+}
+
+impl KafkaProducer {
+    pub fn new(config: KafkaConsumerConfig) -> Result<Self> {
+        let mut client_config = rdkafka::config::ClientConfig::new();
+        client_config.set("bootstrap.servers", &config.brokers);
+
+        if let Some(message_timeout_ms) = config.message_timeout_ms {
+            client_config.set("message.timeout.ms", message_timeout_ms.to_string());
+        }
+        if let Some(message_max_size) = config.message_max_size {
+            client_config.set("message.max.bytes", message_max_size.to_string());
+        }
+
+        #[cfg(feature = "ssl")]
+        match &config.security_config {
+            #[cfg(feature = "ssl")]
+            Some(crate::config::SecurityConfig::Ssl(config)) => {
+                client_config
+                    .set("security.protocol", &config.security_protocol)
+                    .set("ssl.ca.location", &config.ssl_ca_location)
+                    .set("ssl.key.location", &config.ssl_key_location)
+                    .set("ssl.certificate.location", &config.ssl_certificate_location);
+
+                if let Some(enable_verification) = config.enable_ssl_certificate_verification {
+                    client_config.set(
+                        "enable.ssl.certificate.verification",
+                        if enable_verification { "true" } else { "false" },
+                    );
+                }
+            }
+            None => {}
+        }
+
+        let producer = client_config.create()?;
+
+        Ok(Self { producer, config })
+    }
+    async fn handle_block(&self, block_stuff: &BlockStuff) -> Result<()> {
+        let block_id = block_stuff.id();
+
+        let extra = block_stuff.load_extra()?;
+        let account_blocks = extra.account_blocks.load()?;
+
+        let mut transactions = Vec::new();
+        for account_block in account_blocks.iter() {
+            let (addr, _, block) = account_block?;
+            for transaction in block.transactions.iter() {
+                let (_, _, transaction) = transaction?;
+                let hash = CellBuilder::build_from(transaction.clone())?
+                    .repr_hash()
+                    .0
+                    .to_vec();
+                let transaction = transaction.load()?;
+                let timestamp = transaction.now;
+                let data = BocRepr::encode(&transaction)?;
+
+                let partition = if block_id.is_masterchain() {
+                    0
+                } else {
+                    // first 3 bits of the account id
+                    (1 + (addr[0] >> 5)) & 0b111
+                };
+
+                transactions.push(TransactionToKafka {
+                    hash,
+                    data,
+                    partition,
+                    timestamp,
+                });
+            }
+        }
+
+        let mut futures: FuturesOrdered<_> = transactions
+            .into_iter()
+            .map(move |tx| self.send_with_retry(tx))
+            .collect();
+
+        while futures.next().await.is_some() {}
+
+        Ok(())
+    }
+
+    async fn send_with_retry(&self, record: TransactionToKafka) {
+        let mut attempt = 0;
+        loop {
+            let record = FutureRecord::to(&self.config.topic)
+                .key(&record.hash)
+                .payload(&record.data)
+                .timestamp((record.timestamp as i64) * 1_000) // milliseconds
+                .partition(record.partition as i32);
+            let queue_timeout = Duration::from_millis(self.config.session_timeout_ms as u64);
+            match self.producer.send(record, queue_timeout).await {
+                Ok(_) => return,
+                Err((error, _)) => {
+                    attempt += 1;
+                    tracing::warn!("Failed to send message (attempt {attempt}): {error:?}",);
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                }
+            }
+        }
+    }
+}
+
+struct TransactionToKafka {
+    hash: Vec<u8>,
+    data: Vec<u8>,
+    partition: u8,
+    timestamp: u32,
+}
+
+impl StateSubscriber for KafkaProducer {
+    type HandleStateFut<'a> = BoxFuture<'a, Result<()>>;
+
+    fn handle_state<'a>(&'a self, cx: &'a StateSubscriberContext) -> Self::HandleStateFut<'a> {
+        async move {
+            self.handle_block(&cx.block).await?;
+            Ok(())
+        }
+        .boxed()
+    }
+}
