2729fcdfc5c4b4048050ef2442f8b4b9f4131e55
Alexey Pashinov <pashinov93@gmail.com>
2025-01-18 15:31:01 +0100

    upload archives to S3

--- archives-node/src/subscriber.rs
+++ archives-node/src/subscriber.rs
@@ -1,21 +1,62 @@
 use anyhow::Result;
 use futures_util::future::BoxFuture;
 use futures_util::FutureExt;
+use object_store::gcp::{GoogleCloudStorage, GoogleCloudStorageBuilder};
+use object_store::path::Path;
+use object_store::{ObjectStore, WriteMultipart};
 use tycho_core::block_strider::{ArchiveSubscriber, ArchiveSubscriberContext};
 
 use crate::config::ArchiveUploaderConfig;
 
 pub struct ArchiveUploader {
-    _config: ArchiveUploaderConfig,
+    config: ArchiveUploaderConfig,
+    gcs: GoogleCloudStorage,
 }
 
 impl ArchiveUploader {
     pub fn new(config: ArchiveUploaderConfig) -> Result<Self> {
-        Ok(ArchiveUploader { _config: config })
+        let gcs = GoogleCloudStorageBuilder::new()
+            .with_bucket_name("archives")
+            .with_application_credentials(&config.credentials_path)
+            .build()?;
+
+        Ok(ArchiveUploader { config, gcs })
     }
 
-    async fn handle_archive(&self, _cx: &ArchiveSubscriberContext<'_>) -> Result<()> {
-        // TODO
+    async fn handle_archive(&self, cx: &ArchiveSubscriberContext<'_>) -> Result<()> {
+        let archive_id = cx.archive_id.to_string();
+        let instance_id = hex::encode(cx.storage.node_state().load_instance_id());
+
+        let location = format!("{instance_id}/{archive_id}");
+
+        let mut attempts = 0;
+
+        // Try to load until we load without retry limit
+        loop {
+            attempts += 1;
+
+            let upload = match self.gcs.put_multipart(&Path::from(location.clone())).await {
+                Ok(upload) => upload,
+                Err(e) => {
+                    tracing::error!(attempts, "failed to put multipart: {e}");
+                    tokio::time::sleep(self.config.retry_delay).await;
+                    continue;
+                }
+            };
+
+            let mut writer = WriteMultipart::new(upload);
+            for (_, chunk) in cx.archive_iterator() {
+                writer.write(&chunk);
+            }
+
+            match writer.finish().await {
+                Ok(_) => break,
+                Err(e) => {
+                    tracing::error!(attempts, "failed to upload archive: {e}");
+                    tokio::time::sleep(self.config.retry_delay).await;
+                }
+            }
+        }
 
         Ok(())
     }
@@ -29,7 +70,7 @@ impl ArchiveSubscriber for ArchiveUploader {
         cx: &'a ArchiveSubscriberContext<'_>,
     ) -> Self::HandleArchiveFut<'a> {
         async move {
-            self.handle_archive(&cx).await?;
+            self.handle_archive(cx).await?;
             Ok(())
         }
         .boxed()
@@ -54,7 +95,7 @@ impl ArchiveSubscriber for OptionalArchiveSubscriber {
     ) -> Self::HandleArchiveFut<'a> {
         match self {
             OptionalArchiveSubscriber::ArchiveUploader(uploader) => {
-                futures_util::future::Either::Left(uploader.handle_archive(&cx).boxed())
+                futures_util::future::Either::Left(uploader.handle_archive(cx).boxed())
             }
             OptionalArchiveSubscriber::BlackHole => {
                 futures_util::future::Either::Right(futures_util::future::ok(()))
