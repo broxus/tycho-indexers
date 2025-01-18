dea582c536fbff9ec9606eba7e7612b02a2e6401
Alexey Pashinov <pashinov93@gmail.com>
2025-01-18 16:43:54 +0100

    spawn uploading archive

--- archives-node/src/subscriber.rs
+++ archives-node/src/subscriber.rs
@@ -1,64 +1,124 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use futures_util::future::BoxFuture;
 use futures_util::FutureExt;
 use object_store::gcp::{GoogleCloudStorage, GoogleCloudStorageBuilder};
 use object_store::path::Path;
 use object_store::{ObjectStore, WriteMultipart};
+use tokio::task::JoinHandle;
+use tracing::Instrument;
 use tycho_core::block_strider::{ArchiveSubscriber, ArchiveSubscriberContext};
+use tycho_util::metrics::HistogramGuard;
+use tycho_util::sync::CancellationFlag;
 
 use crate::config::ArchiveUploaderConfig;
 
 pub struct ArchiveUploader {
+    gcs: Arc<GoogleCloudStorage>,
     config: ArchiveUploaderConfig,
-    gcs: GoogleCloudStorage,
+    prev_archive_upload: tokio::sync::Mutex<Option<UploadArchiveTask>>,
 }
 
 impl ArchiveUploader {
     pub fn new(config: ArchiveUploaderConfig) -> Result<Self> {
-        let gcs = GoogleCloudStorageBuilder::new()
-            .with_bucket_name("archives")
-            .with_application_credentials(&config.credentials_path)
-            .build()?;
+        let gcs = Arc::new(
+            GoogleCloudStorageBuilder::new()
+                .with_bucket_name(&config.bucket_name)
+                .with_application_credentials(&config.credentials_path)
+                .build()?,
+        );
 
-        Ok(ArchiveUploader { config, gcs })
+        Ok(ArchiveUploader {
+            gcs,
+            config,
+            prev_archive_upload: Default::default(),
+        })
     }
 
     async fn handle_archive(&self, cx: &ArchiveSubscriberContext<'_>) -> Result<()> {
-        let archive_id = cx.archive_id.to_string();
-        let instance_id = hex::encode(cx.storage.node_state().load_instance_id());
+        let mut prev_archive_upload = self.prev_archive_upload.lock().await;
 
-        let location = format!("{instance_id}/{archive_id}");
-
-        let mut attempts = 0;
-
-        // Try to load until we load without retry limit
-        loop {
-            attempts += 1;
-
-            let upload = match self.gcs.put_multipart(&Path::from(location.clone())).await {
-                Ok(upload) => upload,
-                Err(e) => {
-                    tracing::error!(attempts, "failed to put multipart: {e}");
-                    tokio::time::sleep(self.config.retry_delay).await;
-                    continue;
-                }
-            };
-
-            let mut writer = WriteMultipart::new(upload);
-            for (_, chunk) in cx.archive_iterator() {
-                writer.write(&chunk);
-            }
-
-            match writer.finish().await {
-                Ok(_) => break,
-                Err(e) => {
-                    tracing::error!(attempts, "failed to upload archive: {e}");
-                    tokio::time::sleep(self.config.retry_delay).await;
-                }
-            }
+        // NOTE: Wait on reference to make sure that the task is cancel safe
+        if let Some(task) = &mut *prev_archive_upload {
+            // Wait upload archive
+            task.finish().await?;
         }
+        *prev_archive_upload = Some(self.spawn_upload_archive(cx));
 
         Ok(())
+    }
+
+    fn spawn_upload_archive(&self, cx: &ArchiveSubscriberContext<'_>) -> UploadArchiveTask {
+        let cancelled = CancellationFlag::new();
+
+        let handle = tokio::task::spawn({
+            let gcs = self.gcs.clone();
+            let archive_id = cx.archive_id;
+            let storage = cx.storage.clone();
+            let location = cx.archive_id.to_string();
+            let retry_delay = self.config.retry_delay;
+
+            let cancelled = cancelled.clone();
+
+            async move {
+                let histogram = HistogramGuard::begin("tycho_storage_upload_archive_time");
+
+                tracing::info!("started");
+                let guard = scopeguard::guard((), |_| {
+                    tracing::warn!("cancelled");
+                });
+
+                let mut attempts = 0;
+
+                // Try to load until we load without retry limit
+                loop {
+                    attempts += 1;
+
+                    let upload = match gcs.put_multipart(&Path::from(location.clone())).await {
+                        Ok(upload) => upload,
+                        Err(e) => {
+                            tracing::error!(attempts, "failed to put multipart: {e}");
+                            tokio::time::sleep(retry_delay).await;
+                            continue;
+                        }
+                    };
+
+                    let mut writer = WriteMultipart::new(upload);
+                    for (_, chunk) in storage.block_storage().archive_chunks_iterator(archive_id) {
+                        anyhow::ensure!(!cancelled.check(), "task aborted");
+                        writer.write(&chunk);
+                    }
+
+                    match writer.finish().await {
+                        Ok(_) => break,
+                        Err(e) => {
+                            tracing::error!(attempts, "failed to upload archive: {e}");
+                            tokio::time::sleep(retry_delay).await;
+                        }
+                    }
+                }
+
+                // Done
+                scopeguard::ScopeGuard::into_inner(guard);
+                tracing::info!(
+                    elapsed = %humantime::format_duration(histogram.finish()),
+                    "finished"
+                );
+
+                Ok(())
+            }
+            .instrument(tracing::info_span!(
+                "spawn_upload_archive",
+                archive_id = cx.archive_id
+            ))
+        });
+
+        UploadArchiveTask {
+            cancelled,
+            handle: Some(handle),
+            archive_id: cx.archive_id,
+        }
     }
 }
 
@@ -77,7 +137,6 @@ impl ArchiveSubscriber for ArchiveUploader {
     }
 }
 
-#[expect(clippy::large_enum_variant, reason = "doesn't matter")]
 pub enum OptionalArchiveSubscriber {
     ArchiveUploader(ArchiveUploader),
     BlackHole,
@@ -100,6 +159,48 @@ impl ArchiveSubscriber for OptionalArchiveSubscriber {
             OptionalArchiveSubscriber::BlackHole => {
                 futures_util::future::Either::Right(futures_util::future::ok(()))
             }
+        }
+    }
+}
+
+struct UploadArchiveTask {
+    archive_id: u32,
+    cancelled: CancellationFlag,
+    handle: Option<JoinHandle<Result<()>>>,
+}
+
+impl UploadArchiveTask {
+    async fn finish(&mut self) -> Result<()> {
+        // NOTE: Await on reference to make sure that the task is cancel safe
+        if let Some(handle) = &mut self.handle {
+            if let Err(e) = handle
+                .await
+                .map_err(|e| {
+                    if e.is_panic() {
+                        std::panic::resume_unwind(e.into_panic());
+                    }
+                    anyhow::Error::from(e)
+                })
+                .and_then(std::convert::identity)
+            {
+                tracing::error!(
+                    archive_id = self.archive_id,
+                    "failed to commit archive: {e:?}"
+                );
+            }
+
+            self.handle = None;
+        }
+
+        Ok(())
+    }
+}
+
+impl Drop for UploadArchiveTask {
+    fn drop(&mut self) {
+        self.cancelled.cancel();
+        if let Some(handle) = &self.handle {
+            handle.abort();
         }
     }
 }
