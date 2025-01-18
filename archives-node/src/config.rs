a9dfba59eec7fc116cfa9645d76ea768766739b7
Alexey Pashinov <pashinov93@gmail.com>
2025-01-18 15:31:01 +0100

    upload archives to S3

--- archives-node/src/config.rs
+++ archives-node/src/config.rs
@@ -1,16 +1,28 @@
+use std::time::Duration;
+
 use serde::{Deserialize, Serialize};
+use tycho_util::serde_helpers;
 
 #[derive(Debug, Clone, Deserialize, Serialize, Default)]
 pub struct UserConfig {
     pub s3_uploader: Option<ArchiveUploaderConfig>,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
+#[derive(Default, Debug, Clone, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-pub struct ArchiveUploaderConfig {}
+pub struct ArchiveUploaderConfig {
+    pub credentials_path: String,
+
+    /// Default: 1 seconds.
+    #[serde(with = "serde_helpers::humantime")]
+    pub retry_delay: Duration,
+}
 
 impl Default for ArchiveUploaderConfig {
     fn default() -> Self {
-        Self {}
+        Self {
+            credentials_path: Default::default(),
+            retry_delay: Duration::from_secs(1),
+        }
     }
 }
