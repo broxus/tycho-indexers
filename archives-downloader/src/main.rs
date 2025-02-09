d3f97db4e241ebaf9acc8ee896665cd2faaeb686
Alexey Pashinov <pashinov93@gmail.com>
2025-02-09 18:02:07 +0100

    use metrics from tycho-utils to remove duplicated code

--- archives-downloader/src/main.rs
+++ archives-downloader/src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Context;
 use clap::Parser;
-use tikv_jemalloc_ctl::{epoch, stats};
 use tycho_core::block_strider::{ColdBootType, PsSubscriber, ShardStateApplier};
 use tycho_util::cli::logger::init_logger;
+use tycho_util::cli::metrics::spawn_allocator_metrics_loop;
 
 use crate::config::UserConfig;
 use crate::provider::{ArchiveBlockProvider, ArchiveBlockProviderConfig};
@@ -45,7 +45,6 @@ async fn main() -> anyhow::Result<()> {
     if config.metrics.is_some() {
         spawn_allocator_metrics_loop();
     }
-    spawn_allocator_metrics_loop();
 
     let mut node = args.node.create(config.clone()).await?;
 
@@ -75,67 +74,4 @@ async fn main() -> anyhow::Result<()> {
         .await?;
 
     Ok(tokio::signal::ctrl_c().await?)
-}
-
-macro_rules! set_metrics {
-    ($($metric_name:expr => $metric_value:expr),* $(,)?) => {
-        $(
-            metrics::gauge!($metric_name).set($metric_value as f64);
-        )*
-    };
-}
-
-pub fn spawn_allocator_metrics_loop() {
-    tokio::spawn(async move {
-        loop {
-            let s = match fetch_stats() {
-                Ok(s) => s,
-                Err(e) => {
-                    tracing::error!("failed to fetch jemalloc stats: {e}");
-                    return;
-                }
-            };
-
-            set_metrics!(
-                "jemalloc_allocated_bytes" => s.allocated,
-                "jemalloc_active_bytes" => s.active,
-                "jemalloc_metadata_bytes" => s.metadata,
-                "jemalloc_resident_bytes" => s.resident,
-                "jemalloc_mapped_bytes" => s.mapped,
-                "jemalloc_retained_bytes" => s.retained,
-                "jemalloc_dirty_bytes" => s.dirty,
-                "jemalloc_fragmentation_bytes" => s.fragmentation,
-            );
-            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        }
-    });
-}
-
-pub struct JemallocStats {
-    pub allocated: u64,
-    pub active: u64,
-    pub metadata: u64,
-    pub resident: u64,
-    pub mapped: u64,
-    pub retained: u64,
-    pub dirty: u64,
-    pub fragmentation: u64,
-}
-
-fn fetch_stats() -> anyhow::Result<JemallocStats, tikv_jemalloc_ctl::Error> {
-    // Stats are cached. Need to advance epoch to refresh.
-    epoch::advance()?;
-
-    Ok(JemallocStats {
-        allocated: stats::allocated::read()? as u64,
-        active: stats::active::read()? as u64,
-        metadata: stats::metadata::read()? as u64,
-        resident: stats::resident::read()? as u64,
-        mapped: stats::mapped::read()? as u64,
-        retained: stats::retained::read()? as u64,
-        dirty: (stats::resident::read()?
-            .saturating_sub(stats::active::read()?)
-            .saturating_sub(stats::metadata::read()?)) as u64,
-        fragmentation: (stats::active::read()?.saturating_sub(stats::allocated::read()?)) as u64,
-    })
 }
